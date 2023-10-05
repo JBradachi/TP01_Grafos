4b380e5ebbd0482af388ec10057a5b5cd6d3a3c2
João Bradachi <joao.bradachi@ufv.br>
2023-10-05 19:49:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 # importando as dependencias
 import matplotlib.pyplot as plt
+import os 
 import networkx as nx
 
+nomeDoArquivo = str(input("Insira o nome do arquivo (sem a extensão .graphml) >>> "))
+nomeDoArquivo = nomeDoArquivo+".graphml"
+
 # lendo o arquivo hml
-G = nx.read_graphml(path="grafoTeste2.graphml")
+G = nx.read_graphml(path=nomeDoArquivo)
 
-# Ordem do grafo
-ordem = nx.number_of_nodes(G)
-print(f"Ordem do grafo: {ordem}\n")
+while(1):
+    os.system('clear')
+    print("="*50)
+    print("Escolha uma das opções abaixo: \n1 - Mostrar grafo na tela\n2 - Exibir a ordem do grafo\n3 - Exibir o tamanho do grafo\n4 - Inserir outro grafo\n5 - Sair do programa")
+    numMenu = int(input(">>> "))
 
-
-# Tamanho do grafo
-tamanho = nx.number_of_edges(G)
-print(f"Tamanho do grafo: {tamanho}\n")
+    if numMenu == 1:
+        # mostra grafo na tela bonitinho 
+        pos = nx.spring_layout(G, seed=200)
+        nx.draw(G, pos)
+        plt.show()
+        
+    elif numMenu == 2:
+        # Tamanho do grafo
+        tamanho = nx.number_of_edges(G)
+        print(f"Tamanho do grafo: {tamanho}\n")
+        pause = str(input("Pressione enter para prosseguir"))
+        
+    elif numMenu == 3:
+        # Ordem do grafo
+        ordem = nx.number_of_nodes(G)
+        print(f"Ordem do grafo: {ordem}\n")
+        pause = str(input("Pressione enter para prosseguir"))
+        
+    elif numMenu == 4:
+        nomeDoArquivo = str(input("Insira o nome do arquivo (sem a extensão .graphml) >>> "))
+        nomeDoArquivo = nomeDoArquivo+".graphml"
+        
+        # lendo o arquivo hml
+        G = nx.read_graphml(path=nomeDoArquivo)
+        
+    elif numMenu == 5:
+        os.system('clear')
+        break
 
 
 # mostra grafo na tela bonitinho 

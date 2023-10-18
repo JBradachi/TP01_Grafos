af3fbe58d4bd2259e27fce6d8b1559637dcba3e0
João Bradachi <joao.bradachi@ufv.br>
2023-10-18 15:10:01 -0300

    add + teste

--- main.py
+++ main.py
@@ -33,8 +33,27 @@ try:
 
         if numMenu == 1:
             # mostra grafo na tela 
-            pos = nx.spring_layout(G, seed=200)
-            nx.draw(G, pos)
+            pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+
+            # nodes
+            nx.draw_networkx_nodes(G, pos, node_size=700)
+
+            # edges
+            
+            nx.draw_networkx_edges(
+                G, pos, width=6, alpha=0.5, edge_color="b", style="dashed"
+            )
+
+            # node labels
+            nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+            # edge weight labels
+            edge_labels = nx.get_edge_attributes(G, "weight")
+            nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
+            ax = plt.gca()
+            ax.margins(0.08)
+            plt.axis("off")
+            plt.tight_layout()
             plt.show()
             
         elif numMenu == 2:
